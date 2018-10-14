f0a3113091267920eb7a035e7c43ea99d84faed9
andrewgwear <33969968+andrewgwear@users.noreply.github.com>
2018-10-14 21:49:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-
-myFunction () {
- .navBar li display:inline-block; 
+function myFunction() {
+ var x = document.getElementByClass('hider');
+ if (x.style.display === "none") {
+  x.style.display = "block";
+ }
+ else {
+  x.style.display = 'none';
+ }
 }
